89795a6f3374e5aa8c64476e99fe4edab63a9de7
juanjoav <57376589+juanjoav@users.noreply.github.com>
2020-09-12 11:34:40 -0500

    v1.2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("holi");
+        System.out.println("esta vuelta si sirve");
+        System.out.println("agradecido con el de arriba");
     }
 }
